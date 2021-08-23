e9267821d571061bed587ffa817429df95d9a1da
Daniel Johnson <danjohnson77@gmail.com>
2021-08-23 16:49:28 -0700

    added fade to menu

--- src/components/pages/menu/Menu.js
+++ src/components/pages/menu/Menu.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useParams } from "react-router-dom";
-
+import ObserverAnimation from "../../layout/ObserverAnimation";
 import MenuSelect from "./content/MenuSelect";
 import SubMenu from "./content/SubMenu";
 
@@ -11,7 +11,13 @@ const Menu = () => {
   menuId = Object.entries(menuId).length > 0 ? menuId : false;
   const id = menuId.menuId;
 
-  return id ? <SubMenu menu={id} /> : <MenuSelect />;
+  return id ? (
+    <SubMenu menu={id} />
+  ) : (
+    <ObserverAnimation animation="fade-in" customClasses="">
+      <MenuSelect />
+    </ObserverAnimation>
+  );
 };
 
 export default Menu;
